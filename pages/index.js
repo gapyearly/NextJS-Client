e79eff0719b94609332f3876b74c6d79f53a3a01
Arlene Wang <75002440+wangarlene@users.noreply.github.com>
2021-03-27 15:55:33 -0400

    font/color added

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,9 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico"/>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com"/>
+        <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet"></link>
       </Head>
 
       <main className={styles.main}>
