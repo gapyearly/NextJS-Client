2fc4dbc44aa0c405b00cf2461fd93f0eecbd29f6
NukeWolf <smart.alex.huang@gmail.com>
2021-03-28 15:43:30 -0400

    Update Navmenu

--- components/Layouts/Navbar/Navmenu.js
+++ components/Layouts/Navbar/Navmenu.js
@@ -1,9 +1,16 @@
 const sitemap = require("./sitemap.json");
 
 export default function NavMenu() {
-  const navItems = sitemap.map(() => {});
+  const navItems = sitemap.map((navItemInfo) => {
+    return <NavItem key={navItemInfo.label} data={navItemInfo} />;
+  });
+  return <div>{navItems}</div>;
 }
 
-const NavItem = ({dropDown Items}) => {
-    
-}
+const NavItem = ({ data }) => {
+  return (
+    <div>
+      <a>{data.label}</a>
+    </div>
+  );
+};
