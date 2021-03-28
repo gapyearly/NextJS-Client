6a6e1d12c2558cdc6e2e683755ce9576ed404123
NukeWolf <smart.alex.huang@gmail.com>
2021-03-28 11:43:28 -0400

    Nav is now fixed, implemented layout component

--- components/Layouts/Navbar/index.js
+++ components/Layouts/Navbar/index.js
@@ -1,30 +1,25 @@
-import Link from 'next/link'
-import styles from './Navbar.module.css'
+import Link from "next/link";
+import styles from "./Navbar.module.css";
 
 export default function Navbar() {
     return (
-        <div id='navbar' className={styles.navbar}>
-            <Link href='/'>
-                <img
-                    src='/images/logo.png'
-                    layout='fill'
-                    alt='Gapyearly Logo'
-                    className={styles.logo}
-                />
-            </Link>
-            <NavMenu>
-                
-            </NavMenu>
-            
-        </div>
-    )
+        <nav>
+            <div id="navbar" className={styles.navbar}>
+                <Link href="/">
+                    <img
+                        src="/images/logo.png"
+                        layout="fill"
+                        alt="Gapyearly Logo"
+                        className={styles.logo}
+                    />
+                </Link>
+                <NavMenu></NavMenu>
+            </div>
+        </nav>
+    );
 }
 
 const NavMenu = () => {
-    return(
-        <div></div>
-    )
-}
-const DropDownItem = () =>{
-
-}
+    return <div></div>;
+};
+const DropDownItem = () => {};
--- pages/componentTesting.js
+++ pages/componentTesting.js
@@ -1,14 +1,15 @@
-/** 
- * 
- * 
-*/
-import Navbar from '@components/Layouts/Navbar'
+/**
+ *
+ *
+ */
+import Layout from "@components/Layouts/Layout";
 
-import redirect from 'nextjs-redirect'
+import redirect from "nextjs-redirect";
 export default function Home() {
-    const Redirect = redirect('/404')
-    if(process.env.NODE_ENV === 'production'){return <Redirect></Redirect>};
+    const Redirect = redirect("/404");
+    if (process.env.NODE_ENV === "production") {
+        return <Redirect></Redirect>;
+    }
 
-
-    return <Navbar/>;
+    return <Layout />;
 }
