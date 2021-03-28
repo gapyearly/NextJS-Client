07a0f033aa32cb04e4e26920bbe60e594ed1d7ae
Arlene Wang <75002440+wangarlene@users.noreply.github.com>
2021-03-28 17:51:56 -0400

    navbar spacing

--- components/Layouts/Navbar/Navmenu.js
+++ components/Layouts/Navbar/Navmenu.js
@@ -5,7 +5,7 @@ export default function NavMenu() {
   const navItems = sitemap.map((navItemInfo) => {
     return <NavItem key={navItemInfo.label} data={navItemInfo} />;
   });
-  return <div>{navItems}</div>;
+  return <div className={styles.navItems}>{navItems}</div>;
 }
 
 const NavItem = ({ data }) => {
