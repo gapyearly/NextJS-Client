959500bcd8ac981396b9e6e2550b5b509db1bd31
NukeWolf <smart.alex.huang@gmail.com>
2021-03-28 18:06:44 -0400

    Starting navmenu

--- components/Layouts/Navbar/Navmenu.js
+++ components/Layouts/Navbar/Navmenu.js
@@ -3,11 +3,15 @@ const sitemap = require("./sitemap.json").header;
 
 export default function NavMenu() {
   const navItems = sitemap.map((navItemInfo) => {
-    return <NavItem key={navItemInfo.label} data={navItemInfo} />;
+    return <NavFolder key={navItemInfo.label} data={navItemInfo} />;
   });
   return <div>{navItems}</div>;
 }
 
-const NavItem = ({ data }) => {
-  return <a className={styles.navLink}>{data.label}</a>;
+const NavFolder = ({ data }) => {
+  return (
+    <span>
+      <a className={styles.navLink}>{data.label}</a>
+    </span>
+  );
 };
