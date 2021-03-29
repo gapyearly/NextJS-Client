683821b52933f3e217b602e12bac189fd585f77e
NukeWolf <smart.alex.huang@gmail.com>
2021-03-29 18:51:09 -0400

    updated navmenu description

--- components/Layouts/Navbar/Navmenu.js
+++ components/Layouts/Navbar/Navmenu.js
@@ -2,9 +2,15 @@ import styles from "./Navbar.module.css";
 import Link from "next/link";
 
 const sitemap = require("./sitemap.json").header;
-
+/**
+ * Sitemap Header Structure Ex.
+ * Array of navFolders:
+ *    Label:"About"  // Title of nav folder
+ *    dropDownItems: Array of dropdownItems // Each dropdown item consists of:
+ *       "linkLabel" to be displayed and,
+ *       "href" to be used as a link
+ */
 export default function NavMenu() {
-
   const navFolders = sitemap.map((navFolderInfo) => {
     return <NavFolder key={navFolderInfo.label} data={navFolderInfo} />;
   });
