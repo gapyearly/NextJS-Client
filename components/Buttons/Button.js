286da5eeabf63f9517a7f666a1075dfdcefe7a7d
Arlene Wang <75002440+wangarlene@users.noreply.github.com>
2021-03-30 15:10:17 -0400

    buttons

---
+++ components/Buttons/Button.js
@@ -0,0 +1,8 @@
+import Link from "next/link";
+// import styles from "Button.module.css"
+
+export default function Button ({buttonColor, buttonLink, children}) {
+    return <button className={buttonColor}>
+        <Link href={buttonLink}><a className="noUnderline">{children}</a></Link>
+    </button>
+}
