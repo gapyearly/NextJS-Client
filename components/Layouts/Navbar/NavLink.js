924818018ce87874c7e0daf181a6c6c0a59e1d43
Arlene Wang <75002440+wangarlene@users.noreply.github.com>
2021-03-31 01:18:14 -0400

    link, nav updates

---
+++ components/Layouts/Navbar/NavLink.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Link from "next/link";
+import styles from "./Navbar.module.css"
+
+export default function NavLink({ href, children }) {
+  return (
+    <Link href={href}>
+      {typeof Children === "string" ? <a className={styles.dropdownItem}>{children}</a> : children}
+      {/* TODO: styles not being applied */}
+    </Link>
+  );
+}
