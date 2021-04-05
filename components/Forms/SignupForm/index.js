57eec5c840309b18465d3b13c5d104511d4db624
NukeWolf <smart.alex.huang@gmail.com>
2021-04-04 23:12:59 -0400

    Started blog templating, changed signupform

--- components/Forms/SignupForm/index.js
+++ components/Forms/SignupForm/index.js
@@ -1,17 +1,10 @@
 import { useForm, useStep } from "react-hooks-helper";
 import styles from "./SignupForm.module.css";
+import Layout from "@components/Layouts/SignupLayout";
 
 export default function SignupForm() {
-  const [formData, setForm] = useForm(defaultData);
-  const { step, navigation } = useStep({ initialStep: 0, steps });
-  const { id } = step;
-  return (
-    <main className={styles.background}>
-      <img
-        src="/images/greenblue-valleys.svg"
-        alt="Background"
-        className={styles.backgroundImage}
-      />
-    </main>
-  );
+  // const [formData, setForm] = useForm(defaultData);
+  // const { step, navigation } = useStep({ initialStep: 0, steps });
+  // const { id } = step;
+  return <Layout src="/images/greenblue-valleys.svg"></Layout>;
 }
--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,3 +1,31 @@
+import strapi from "@api/strapi";
+
 export default function Article() {
   return <div></div>;
+}
+
+export async function getStaticProps({ params }) {
+  // We have the required page data, pass it to the page component
+  const fullSlug = params.slug.join("/");
+  const { data: page } = await strapi.get(`/pages?slug=${fullSlug}`);
+
+  return {
+    props: {
+      pageInfo: page[0],
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  // Get all pages from Strapi
+  const { data: pages } = await strapi.get("blogs");
+
+  const paths = pages.map((page) => {
+    // Decompose the slug that was saved in Strapi
+    const slugArray = page.slug.split("/");
+    return {
+      params: { slug: slugArray },
+    };
+  });
+  return { paths, fallback: false };
 }
