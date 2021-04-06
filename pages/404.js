e85afb342b1ba017bca652077079e4e7ed767424
Arlene Wang <75002440+wangarlene@users.noreply.github.com>
2021-04-05 20:34:56 -0400

    404 page

--- pages/404.js
+++ pages/404.js
@@ -1,5 +1,15 @@
 import Layout from "@components/Layouts/Layout";
-
+import Link from "next/link";
+import { FaRegFrown } from "react-icons/fa";
 export default function Error404() {
-  return <Layout>404</Layout>;
+  return (
+    <Layout>
+      <div className="error404">
+        <FaRegFrown className="sadIcon" size="15rem"></FaRegFrown>
+        <h1>404</h1>
+        <h2> Oh no! Page not found.</h2>
+        <Link href="/">Return to home?</Link>
+      </div>
+    </Layout>
+  );
 }
--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,12 +1,33 @@
 import strapi from "@api/strapi";
 import Layout from "@components/Layouts/Layout";
+
+import PageTitle from "@components/PageTitle";
 import RichText from "@components/Sections/RichText";
+import Link from "next/link";
+import { FaArrowLeft } from "react-icons/fa";
+import styles from "@styles/Blog.module.css";
 
 export default function BlogPost({ blogData }) {
-  const { content } = blogData;
+  const { content, title, author, description } = blogData;
+  const fullName = `${author.personalInfo.firstName} ${author.personalInfo.lastName}`;
+
+  console.log(blogData);
   return (
     <Layout>
+      <Link href="/blog" passHref>
+        <div className={styles.clickable}>
+          <FaArrowLeft size="13" /> <a>Back to all posts</a>
+        </div>
+      </Link>
+      <h1 className={styles.title}>{title}</h1>
+      <h2 className={styles.author}>By {fullName}</h2>
       <RichText data={content}></RichText>
+      <Link href="/blog" passHref>
+        <div className={styles.clickable}>
+          <FaArrowLeft size="13" /> <a>Back to all posts</a>
+        </div>
+      </Link>
+      <form></form>
     </Layout>
   );
 }
