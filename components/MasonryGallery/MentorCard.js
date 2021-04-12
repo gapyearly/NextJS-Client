de4d73d343a6c8af573c6a755c94c17956a5ebfd
Arlene Wang <75002440+wangarlene@users.noreply.github.com>
2021-04-12 17:25:55 -0400

    mentor card bg color (to debug)

--- components/MasonryGallery/MentorCard.js
+++ components/MasonryGallery/MentorCard.js
@@ -3,6 +3,7 @@ import Link from "next/link";
 import Button from "@components/Buttons/Button";
 import { HiMail } from "react-icons/hi";
 import ReactHtmlParser from "react-html-parser";
+
 export default function MentorCard({
   firstName,
   lastName,
@@ -10,26 +11,40 @@ export default function MentorCard({
   universityYear,
   summary,
   struggles,
+  bgColor,
 }) {
+  const MentorCardContent = () => {
+    return (
+      <>
+        <div className={styles.mentorCard}>
+          <h2>{firstName} </h2>
+          <h2>{lastName}</h2>
+          <h3>
+            {universityName} {universityYear}
+          </h3>
+          <h4>WHAT I DID:</h4>
+          {ReactHtmlParser(summary)}
+          <h4>WHAT I STRUGGLED WITH:</h4>
+          {ReactHtmlParser(struggles)}
+        </div>
+        <div className={styles.messageBtn}>
+          <Button href="/" color="darkBg">
+            <HiMail></HiMail> Message
+          </Button>
+        </div>
+      </>
+    );
+  };
+  const bgColorComponent = (bgColor) => {
+    return {
+      red: <MentorCardContent className={styles.redBg} />,
+      yellow: <MentorCardContent className={styles.yellowBg} />,
+      green: <MentorCardContent className={styles.greenBg} />,
+      blue: <MentorCardContent className={styles.blueBg} />,
+    }[bgColor];
+  };
+
   return (
-    <div className={styles.mentorCardOverlay}>
-      <div className={styles.mentorCard}>
-        <h2>{firstName} </h2>
-        <h2>{lastName}</h2>
-        <h3>
-          {universityName} {universityYear}
-        </h3>
-        <h4>WHAT I DID:</h4>
-        {ReactHtmlParser(summary)}
-        <h4>WHAT I STRUGGLED WITH:</h4>
-        {ReactHtmlParser(struggles)}
-      </div>
-      <div className={styles.messageBtn}>
-        <Button href="/" color="darkBg">
-          <HiMail></HiMail> Message
-        </Button>
-      </div>
-    </div>
+    <div className={styles.mentorCardOverlay}>{bgColorComponent(bgColor)}</div>
   );
 }
-// REPLACE LINKGKDGKFGKDFLGD
