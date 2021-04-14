50409b78c292cfbe023d398c7f7b88501e0ea092
Arlene Wang <75002440+wangarlene@users.noreply.github.com>
2021-04-14 00:03:55 -0400

    loading pages

--- pages/auth/google/index.js
+++ pages/auth/google/index.js
@@ -1,9 +1,14 @@
 // Redirects authentication to backend flow.
 import redirect from "nextjs-redirect";
+import styles from "@styles/Login.module.css";
 
 const Redirect = redirect(
   `${process.env.NEXT_PUBLIC_BACKEND_API}/connect/google`
 );
 export default function GoogleRedirect() {
-  return <Redirect></Redirect>;
+  return (
+    <Redirect>
+      <h1 className={styles.redirect}>Loading...</h1>
+    </Redirect>
+  );
 }
