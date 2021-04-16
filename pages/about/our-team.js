fcac9fc2648f38eb3631c7e0262132352bc6853b
NukeWolf <smart.alex.huang@gmail.com>
2021-04-16 00:35:22 -0400

    Readded styles

--- pages/about/our-team.js
+++ pages/about/our-team.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import styles from "../../styles/Home.module.css";
 import Layout from "@components/Layouts/Layout";
 import PageTitle from "@components/PageTitle";
 import Title from "@components/Title";
