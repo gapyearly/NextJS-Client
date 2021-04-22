146c121516a5ce15f24774b1db0a3b2a5eb6d1e6
Arlene Wang <75002440+wangarlene@users.noreply.github.com>
2021-04-21 22:09:55 -0400

    alex fix this

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,13 @@
 import "../styles/globals.css";
 import "../styles/NProgress.css";
 import "../styles/ckeditor.css";
+import styles from "styles/Alert.module.css";
 // Used for authentication handling, and user info
-
+import { FaCheck, FaExclamationCircle } from "react-icons/fa";
+import { IoInformationCircleOutline, IoClose } from "react-icons/io5";
 import { AuthProvider } from "@contexts/auth";
 
 import { transitions, positions, Provider as AlertProvider } from "react-alert";
-import AlertTemplate from "react-alert-template-basic";
 
 import NProgress from "nprogress";
 import Router from "next/router";
@@ -17,14 +18,33 @@ Router.events.on("routeChangeStart", (url) => {
 });
 Router.events.on("routeChangeComplete", () => NProgress.done());
 Router.events.on("routeChangeError", () => NProgress.done());
+
 function MyApp({ Component, pageProps }) {
+  const AlertTemplate = ({ options, message, close }) => {
+    return (
+      <div className={styles.alert}>
+        {options.type === "info" && <IoInformationCircleOutline />}
+        {options.type === "success" && <FaCheck />}
+        {options.type === "error" && (
+          <FaExclamationCircle
+            value={{ style: { width: 5, marginRight: "15px" } }}
+          />
+        )}
+
+        {message}
+        <button onClick={close}>
+          <IoClose />
+        </button>
+      </div>
+    );
+  };
+
   const options = {
     position: positions.TOP_CENTER,
     // timeout: 5000,
-    offset: "30px",
 
     // you can also just use 'scale'
-    transition: transitions.SCALE,
+    transition: transitions.FADE,
     containerStyle: {
       zIndex: 10002,
     },
