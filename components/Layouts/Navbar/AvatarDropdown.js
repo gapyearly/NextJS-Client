d5e1bc4f7aa6cbac3a867cdb9a3f4a875a878ba1
NukeWolf <smart.alex.huang@gmail.com>
2021-04-22 16:04:21 -0400

    Avatar Dropdown Updated

--- components/Layouts/Navbar/AvatarDropdown.js
+++ components/Layouts/Navbar/AvatarDropdown.js
@@ -2,29 +2,54 @@ import { Avatar } from "react-chat-elements";
 import { FaAngleDown } from "react-icons/fa";
 import styles from "./Navbar.module.css";
 import React, { useState } from "react";
+import Navlink from "./NavLink";
+import { useAlert } from "react-alert";
+import { useAuth } from "@contexts/auth";
 
 export default function AvatarDropdown({ avatar }) {
   const [showDropdown, setShowDropdown] = useState(false);
-  console.log(showDropdown);
+
   return (
-    <div
-      className={styles.avatarContainer}
-      onClick={() => {
-        setShowDropdown(!showDropdown);
-      }}
-    >
-      <div></div>
-      <Avatar
-        src={avatar}
-        alt={"logo"}
-        size="small"
-        type="circle"
-        className={styles.avatar}
-      />
-      <FaAngleDown size={"1rem"} />
-      <div className={styles.dropdownContent}>
-        <p>hey</p>
+    <div>
+      <div
+        className={styles.avatarContainer}
+        onClick={() => {
+          setShowDropdown(!showDropdown);
+        }}
+      >
+        <Avatar
+          src={avatar}
+          alt={"logo"}
+          size="small"
+          type="circle"
+          className={styles.avatar}
+        />
+        <FaAngleDown size={"1rem"} />
       </div>
+      {showDropdown && <Dropdown />}
     </div>
   );
 }
+
+const Dropdown = () => {
+  const alert = useAlert();
+  const { logout } = useAuth();
+  // TODO Logout Page?
+  return (
+    <div className={styles.avatarDropdownContent}>
+      <Navlink href="/dashboard">My Dashboard</Navlink>
+      <Navlink href="/dashboard/profile">My Profile</Navlink>
+      <Navlink href="/dashboard/messaging">Messaging</Navlink>
+      <Navlink href="/">Settings</Navlink>
+      <a
+        onClick={() => {
+          alert.success("Logged out");
+          logout();
+        }}
+        href="#"
+      >
+        Logout
+      </a>
+    </div>
+  );
+};
