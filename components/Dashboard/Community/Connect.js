a3683c46af3091634c2168438b283579a00bd366
NukeWolf <smart.alex.huang@gmail.com>
2021-04-25 15:17:56 -0400

    Fix Casing

---
+++ components/Dashboard/Community/Connect.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import styles from "@styles/Dashboard/UserDashboard.module.css";
+
+export default function Connect() {
+  return (
+    <>
+      <h3>Match me in future monthly rounds!</h3>
+      {/* Switch here */}
+      <p>
+        Gapyearly will use your profile info to match you with another gapper.
+        Find out more about matching <Link href="/connect">here</Link>!
+      </p>
+      <h3 className={styles.dateBanner}>Next match releases: April 25th</h3>
+    </>
+  );
+}
