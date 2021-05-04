b54cea2da4228232bfd9c42bf7184f3450db10ad
NukeWolf <smart.alex.huang@gmail.com>
2021-05-04 19:42:24 -0400

    Added zoom feature to profile picture

--- components/Dashboard/Messaging/MessageModal.js
+++ components/Dashboard/Messaging/MessageModal.js
@@ -1,11 +1,18 @@
 import Button from "@components/Buttons/Button";
 import Modal from "@components/Modal";
+import fullName from "@util/fullName";
+import React, { useState } from "react";
+import { useAlert } from "react-alert";
 
-export default function MessageModal({ show }) {
+export default function MessageModal({ show, recipient }) {
+  const [message, setMessage] = useState("");
+  const alert = useAlert();
+  const onSend = () => {};
   return (
     <Modal show="true" title="Start a conversation">
       <div className=""></div>
-      <p>Send your first message to</p>
+      <p>Send your first message to {fullName(recipient)}</p>
+      <br />
       <textarea></textarea>
       <Button color="greenBg">Send</Button>
     </Modal>
