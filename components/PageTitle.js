1b6d796f697e60f6351b5f1fae2a63467ee651b0
NukeWolf <smart.alex.huang@gmail.com>
2021-05-07 23:02:53 -0400

    Update PageTitle.js

--- components/PageTitle.js
+++ components/PageTitle.js
@@ -2,7 +2,18 @@ import { NextSeo } from "next-seo";
 export default function PageTitle({ title }) {
   return (
     <>
-      <NextSeo title={title} opengraph={{ title: `${title} | Gapyearly` }} />
+      <NextSeo
+        title={title}
+        openGraph={{
+          type: "website",
+          url: "https://www.gapyearly.com",
+          site_name: "Gapyearly",
+          images: [{ url: "/images/discover-your-direction.jpg" }],
+          title: `${title} — Welcome to Gapyearly.`,
+          description:
+            "A free gap year guide written by gap year students. See what gap year students have done & plan a life-changing year with our gap year mentors.",
+        }}
+      />
       <header>
         <h1 className="header">{title}</h1>
       </header>
