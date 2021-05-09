c66e0e3b75f68a98008651a8da882d5f8f0fe861
Arlene Wang <75002440+wangarlene@users.noreply.github.com>
2021-05-09 12:51:26 -0400

    help

--- components/Dashboard/Community/ConnectWindow.js
+++ components/Dashboard/Community/ConnectWindow.js
@@ -1,11 +1,44 @@
 import Link from "next/link";
 import styles from "@styles/Dashboard/UserDashboard.module.css";
-
+import Switch from "rc-switch";
+import strapi from "@api/strapi";
+import { useAlert } from "react-alert";
+import { useRouter } from "next/router";
+import { useAuth } from "@contexts/auth";
+import React, { useState } from "react";
+import NProgress from "nprogress";
 export default function Connect() {
+  const { user } = useAuth();
+  const alert = useAlert();
+  const router = useRouter();
+  const [enabled, setEnabled] = useState(false);
+  const onValueChange = (e) => {
+    setEnabled(e.target.value);
+    console.log(setEnabled(e.target.value));
+  };
+
+  const onClick = async (event) => {
+    event.preventDefault();
+    console.log(enabled);
+    try {
+      NProgress.start();
+      await strapi.put(`users/${user.id}`, {
+        connectInfo: { enabled },
+      });
+      alert.success("Matching enabled!");
+      router.push("/dashboard/submission");
+    } catch {
+      alert.error("Could not enable. Please refresh or contact admin.");
+    }
+    NProgress.done();
+  };
+
   return (
     <>
       <h3>Match me in future monthly rounds!</h3>
-      {/* Switch here */}
+
+      <Switch onChange={onValueChange} onClick={onClick} />
+
       <p>
         Gapyearly will use your profile info to match you with another gapper.
         Find out more about matching <Link href="/community/connect">here</Link>
